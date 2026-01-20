28d98df817191abe8ac9b356b8848ce1b5734992
abondarenkog <abondarenkog01@educantabria.es>
2026-01-20 17:17:24 +0100

    Create ArchivoPrincipal.java
    Añado el main al src

---
+++ src/main/java/com/mycompany/archivoprincipal/ArchivoPrincipal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.archivoprincipal;
+
+/**
+ *
+ * @author DAW112
+ */
+public class ArchivoPrincipal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
