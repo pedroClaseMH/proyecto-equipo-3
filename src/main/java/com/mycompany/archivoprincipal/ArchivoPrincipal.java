24ad2b71b71fd6792e1bc8b7fdd83ad22cd74709
David Gómez Santiago <DAW116@iesmhp.local>
2026-01-20 17:24:38 +0100

    añadido los comentarios a la clase principal correctamente

--- src/main/java/com/mycompany/archivoprincipal/ArchivoPrincipal.java
+++ src/main/java/com/mycompany/archivoprincipal/ArchivoPrincipal.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.archivoprincipal;
+package com.mycompany.archivoprincipal;//nombre del paquete donde se crea la clase principal
 
 /**
  *
- * @author DAW112
+ * @author DAW112-> este es el nombre del autor que ha creado la clase 
  */
 public class ArchivoPrincipal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("SOMOS EL EQUIPO 3");//mostramos un mensaje por pantalla
     }
 }
